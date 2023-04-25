865f577cf811e155c6fb8c72118191e0edc0263f
João Victor Saraiva <joaovsaraivasousa@gmail.com>
2023-04-24 21:20:11 -0300

    creating the scope avaliable for player_per_match, using aasm for transaction between statu on player_per_match, creating aassm for players that gave_up from the match, and this event update all players position listed above

--- app/models/player_per_match.rb
+++ app/models/player_per_match.rb
@@ -1,4 +1,6 @@
 class PlayerPerMatch < ApplicationRecord
+  include AASM
+
   belongs_to :player
   belongs_to :match
 
@@ -6,7 +8,22 @@ class PlayerPerMatch < ApplicationRecord
 
   before_create :set_position
 
+  scope :avaliable, -> { where(status: [PlayerPerMatch.statuses[:confirmed], PlayerPerMatch.statuses[:waiting]]) }
   enum status: { confirmed: 0, waiting: 1, gived_up: 2 }
+
+  aasm column: 'status', enum: true do 
+    state :confirmed, initial: true
+    state :waiting
+    state :gived_up
+
+    event :give_up do
+      transitions from: [:confirmed, :waiting], to: :gived_up
+
+      after do 
+        match.player_per_matches.avaliable.where("position > ?", position).update_all("position = position - 1")
+      end
+    end
+  end
 
   def on_waiting_list?
     position > 24
